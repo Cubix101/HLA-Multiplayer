4796be66936402d850bb53b64623fe9f9d34043e
thdude <30702113+thdude@users.noreply.github.com>
2020-12-23 03:25:47 -0800

    Update otherPlayer.lua

--- Addon/game/hlvr_addons/multiplayer/scripts/vscripts/otherPlayer.lua
+++ Addon/game/hlvr_addons/multiplayer/scripts/vscripts/otherPlayer.lua
@@ -1,13 +1,13 @@
-local p_id
-local username
+--local p_id
+--local username
 --dont worry im gonna add more stuff to this function later
-function UpdateLocation(x,y,z)
-    SetAbsOrigin(x,y,z)
-end
+--function UpdateLocation(x,y,z)
+    --SetAbsOrigin(x,y,z)
+--end
 
-function getID(p_id)
-    print("fuck you")
-end
+--function getID(p_id)
+--    print("ass2")
+--end
 
 function onSpawn()
     --set the username, for now all players will just be you. this is only for testing
