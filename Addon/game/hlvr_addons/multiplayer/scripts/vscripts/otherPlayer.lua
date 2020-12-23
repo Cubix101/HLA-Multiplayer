7c3762d9f89f996cb86105f483f108d926c1af31
thdude <30702113+thdude@users.noreply.github.com>
2020-12-23 04:15:37 -0800

    barely got anything done but too tired rn to work on this shit anymore
    i will do more tomorrow ping me if you need anything

--- Addon/game/hlvr_addons/multiplayer/scripts/vscripts/otherPlayer.lua
+++ Addon/game/hlvr_addons/multiplayer/scripts/vscripts/otherPlayer.lua
@@ -1,8 +1,11 @@
 local p_id
 local username
+
+PlayerGoal = Entities:FindByName(nil,"pm_goal")
+
 --dont worry im gonna add more stuff to this function later
 function UpdateLocation(x,y,z)
-    --SetAbsOrigin(x,y,z)
+    SetOrigin(x,y,z)
 end
 
 function getID(p_id)
@@ -13,5 +16,5 @@ function OnSpawn()
     --set the username, for now all players will just be you. this is only for testing
     --update: this does not work. How to get username???
     --print(getStr(name[0]))
-
+    UpdateLocation(500,500,30)
 end
